7c2cf85d6cfe1ee76737fb08a3cda33f7e8c2f73
osimsadulloev8-lgtm <osimsadulloev8@gmail.com>
2026-06-03 08:33:33 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback, useMemo } from "react";
+\import { useState, useEffect, useRef, useCallback, useMemo } from "react";
 import { createClient, RealtimeChannel } from "@supabase/supabase-js";
 
 /* ══════════════════════════════════════════════════════════════════════════════════════
